44fae867ea5e27f2885ff03b4bbfe2b97e8bd1ba
tanchajosephine <tanchajosephine@gmail.com>
2025-07-23 06:42:51 -0700

    am done with variables

---
+++ 2.0 software 1.0/1.0 python basics/day 1/variable.py
@@ -0,0 +1,76 @@
+# # # print('Hello ')
+# # # #   Why do we need variables
+
+# # # name='yosa' 
+# # # print('I am Jes, ilove this name -jes and this name was given to me by my parents')
+# # # print('my name is '+ name)
+
+# # # # variable 
+# # # # anything inside a  single or double qoute is a string
+# # # my_name ="YOSA"
+# # # # number, we have either float or integer
+# # # age=23
+# # # height=3.56
+# # # # this a booleam
+# # # ismarried=True
+# # #  # or it can be false
+# # #  # casting
+# # #  # naming conversion
+# # #  # number
+
+# # # whole_number = 42 
+# # # decimal_number = 3.4567
+# # # complex_number = 3 + 3j
+
+# # # #text
+# # # greetings  = "hello, world"
+# # # single_char = "a"
+
+# # # # boleam
+# # # is_rainning = True
+# # # is_sunny = False
+
+# # # #check the type of any variable
+# # # print(type(whole_number))
+# # # print(type(greetings))
+
+# # # #variable basic example
+# # # #concatenate variable
+# # # first_name="emma"
+# # # last_name="rose"
+
+# # # #this is not addition-> because  we are adding strings instead,so this is called concatenation
+# # # combine_name=first_name + last_name
+# # # print("my full names are" + combine_name)
+# # # print(f"my full names are (combine_name)")
+
+# # name=input("what is your name? ")
+# # print(f"my name is {name}")
+
+# # age=input("what is your age? ")
+# # print(f"(i am {age} years old)")
+
+# # fav_meal=input("what is your fav_meal? ")
+# # print(f"(i love eating {fav_meal}")
+
+# # school=input("what do you school? ")
+# # print(f"i school at {school}")
+
+# # department=input("what is your department? ")
+# # print(f"in the department of {department}")
+
+# # bestfriend=input("who is ur bestfriend")
+# # print(f"my best friend is{bestfriend}")
+
+# # #excercise
+# # #create a calculator for a rectangle
+# length = 10
+# width =5
+
+# area=length * width
+# print(f"the area is {area}")
+
+# exercise temperature converter
+temperature=input("what is ur temperature")
+fahrenheit = (float (temperature) * 9/5) + 32
+print(f"my temperature in celcius is{temperature},and in fahrenheit {fahrenheit}")
