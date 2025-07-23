6976e0565a38a6159265c9fe3da480cc67f77989
tanchajosephine <tanchajosephine@gmail.com>
2025-07-23 06:57:38 -0700

    i have gotten the length of a string

---
+++ 2.0 software 1.0/1.0 python basics/day 1/strings.py
@@ -0,0 +1,16 @@
+#concartenation and interpolation(istring operations in python) 
+first_name="tancha "
+last_name="josephine"
+#concartenation
+full_name= first_name  + last_name
+
+#interpolation
+
+print(f"my fullnames are {full_name}")
+
+laugh = "ha" * 7
+print(laugh)
+
+#length
+message = "hello,python!"
+print(len(message))
