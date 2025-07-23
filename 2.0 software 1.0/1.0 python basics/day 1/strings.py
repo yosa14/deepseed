06dbd70cc6f1c6011634a942307cd92e1df39130
tanchajosephine <tanchajosephine@gmail.com>
2025-07-23 07:15:38 -0700

    i have done string format

--- 2.0 software 1.0/1.0 python basics/day 1/strings.py
+++ 2.0 software 1.0/1.0 python basics/day 1/strings.py
@@ -14,3 +14,18 @@ print(laugh)
 #length
 message = "hello,python!"
 print(len(message))
+
+#string formatting
+name = "alice"
+age =30
+score = 95.5
+
+#method 1
+print(f"hello ,{name}! you are {age} years")
+print(f"your score is {score:.1f}%")
+
+#method 2
+print("hello, {}! you are {} years old.".format(name, age))
+
+#method 3
+print("hello, %s! you are %d years old."% (name,age))
