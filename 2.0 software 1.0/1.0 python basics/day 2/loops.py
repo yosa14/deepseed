ef34ab0446fa76a63fee5239deaf40cfecf0125e
tanchajosephine <tanchajosephine@gmail.com>
2025-07-24 06:26:26 -0700

    am done with for loop

--- 2.0 software 1.0/1.0 python basics/day 2/loops.py
+++ 2.0 software 1.0/1.0 python basics/day 2/loops.py
@@ -1,12 +1,43 @@
-# for loop
-# 1.starting point
-# 2. condition
-# 3. increment or dicrement
-names=["will","loe","kate","kilian"]
-print(names[0])
-print(names[1])
-print(names[2])
-print(names[3])
+# # # # # # for loop
+# # # # # # 1.starting point
+# # # # # # 2. condition
+# # # # # # 3. increment or dicrement
+# # # # # names=["will","loe","kate","kilian" ,"fgbom","rtybom"]
+# # # # # print(names[0])
+# # # # # print(names[1])
+# # # # # print(names[2])
+# # # # # print(names[3])
 
-for name in names:
-    print(name)
+# # # # # for name in names:
+# # # # #     print(name)
+# # # # #     if name.endswith("bom"):
+# # # # #         print(f"we don catch u: {name}")
+# # # # #     else:
+# # # # #          print(f"welcome to the party : {name}")
+
+# # # # # count=1
+# # # # # for name in names:
+# # # # #     print(f"{count}. {name}")
+# # # # #     count = count + 1
+
+# # # # my_names="Josephine"
+# # # # for letter in my_names:
+# # # #     print(f"letter: {letter}")
+ 
+# # # range method in looping
+# # #range stop starts from o
+# # #for i in range(5):
+# # #    print(i)
+
+# #range(start, stop)
+# # for i in range(2,7):
+# #     print(i)
+
+# #range(start, stop, step)
+# for i in range(0, 10, 2):
+#     print(i)
+
+#count down
+for i in range(10, 0, -1):
+    print(f"countdown: {i}")
+print("HAPPY BIRTHDAY✈️")
