aa7aa1aea4cec2cc632b8c1d61299ce7118f5d05
tanchajosephine <tanchajosephine@gmail.com>
2025-07-24 08:14:44 -0700

    password checker

--- 2.0 software 1.0/1.0 python basics/day 2/password.py
+++ 2.0 software 1.0/1.0 python basics/day 2/password.py
@@ -1,12 +1,28 @@
-password= input("enter your password:")
-lower_case=password.islower()
-upper_case=password.isupper()
-numeric=password.isdigit()
-if lower_case :
-    if  upper_case: 
-        if numeric:
-            print("password is strong")
-    else:
-        print("try again")
+# password= input("enter your password: ")
+# lower_case=password.islower()
+# upper_case=password.isupper()
+# numeric=password.isdigit()
+# if lower_case :
+#     if  upper_case: 
+#         if numeric:
+#             print("password is strong")
+#     else:
+#         print("try again")
+# else:
+#     print("please try again!!")
+l=False
+u = False
+d=False
+password = input("enter your password: ")
+for letter in password:
+    if letter.islower():
+        l=True
+    elif letter.isupper():
+        u=True
+    elif letter.isdigit():
+        d = True
+    
+if l and u and d:
+    print("strong password")
 else:
-    print("please try again!!")
+    print("weak password")
